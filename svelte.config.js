a5389ad71ce8765e127542a9bb46a8a3d4a0e3d6
thisisissss <somyown@proton.me>
2026-04-27 07:33:50 +0700

    add github pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-const config = {
+export default {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build'
+    }),
+    paths: {
+      base: '/united-sovereigns'
+    }
   }
 };
-
-export default config;
